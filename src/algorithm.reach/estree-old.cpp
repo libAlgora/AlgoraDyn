122a433b90b13bd08d9ebbe5a042f2e4a5f59d71
Kathrin Hanauer <kal@xaikal.org>
2018-08-10 17:32:52 +0200

    Speedup VertexData::reset.
    Signed-off-by: Kathrin Hanauer <kal@xaikal.org>

--- src/algorithm.reach/estree-old.cpp
+++ src/algorithm.reach/estree-old.cpp
@@ -71,11 +71,15 @@ struct OldESTree::VertexData {
     }
 
     void reset(VertexData *p = nullptr, unsigned int l = UINT_MAX) {
+        for (auto in: inNeighbors) {
+            if (in != nullptr) {
+                inNeighborIndices.resetToDefault(in->vertex);
+            }
+        }
         inNeighbors.clear();
         inNeighborsTimes.clear();
         inNeighborsLost = 0U;
         parentIndex = 0U;
-        inNeighborIndices.resetAll(graphSize);
         level = l;
         if (p != nullptr) {
             inNeighborIndices[p->vertex] = 1U;
